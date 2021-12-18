f809b90b8db8744c94b2110767d1333f5cfb94fd
Scott Wright <scottwright.qrf@gmail.com>
2021-12-18 11:13:23 -0600

    changes

---
+++ team_creator/v_ball/teams_total_points.py
@@ -0,0 +1,45 @@
+#from v_ball.make_teams import newteam1, newteam2, newteam3, newteam4, newteam5
+from v_ball.models import Player 
+
+
+
+red_team_points = 0
+blue_team_points = 0
+black_team_points= 0
+green_team_points= 0
+brown_team_points= 0
+
+players_red = Player.objects.all().filter(team = 1)
+players_blue = Player.objects.all().filter(team = 2)
+players_black = Player.objects.all().filter(team = 3)
+players_green = Player.objects.all().filter(team = 4)
+players_brown = Player.objects.all().filter(team = 5)
+    
+    
+for i in players_red:
+    p = Player.objects.get(player_name = i)
+    pp = p.skill_level
+    red_team_points += pp    
+
+for i in players_blue:
+    p = Player.objects.get(player_name = i)
+    pp = p.skill_level
+    blue_team_points += pp
+
+for i in players_black:
+    p = Player.objects.get(player_name = i)
+    pp = p.skill_level
+    black_team_points += pp    
+        
+for i in players_green:
+    p = Player.objects.get(player_name = i)
+    pp = p.skill_level
+    green_team_points += pp
+
+for i in players_brown:
+    p = Player.objects.get(player_name = i)
+    pp = p.skill_level
+    brown_team_points += pp
+
+
+
