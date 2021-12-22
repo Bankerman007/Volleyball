107f18a57617016ac6846b4c187031bb31fafd99
Scott Wright <scottwright.qrf@gmail.com>
2021-12-22 11:54:17 -0600

    change

--- team_creator/team_creator/wsgi.py
+++ team_creator/team_creator/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_creator.settings')
 
+#application = get_wsgi_application()
+
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/path/to/more/static/files', prefix='more-files/')
