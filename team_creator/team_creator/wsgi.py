e28f4b6bed102ba798a8b3375a32d5ed36cca2b6
Scott Wright <scottwright.qrf@gmail.com>
2021-12-30 22:34:54 -0600

    changes

--- team_creator/team_creator/wsgi.py
+++ team_creator/team_creator/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_creator.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_creator.team_creator.settings')
 
 application = get_wsgi_application()
 
