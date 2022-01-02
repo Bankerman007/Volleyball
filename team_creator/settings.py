b8dd838a2470a77bf9fe03d56651967fce61d927
Scott Wright <scottwright.qrf@gmail.com>
2022-01-02 15:14:05 -0600

    changes

--- team_creator/settings.py
+++ team_creator/settings.py
@@ -88,9 +88,8 @@ DATABASES = {
     }
 }
 import dj_database_url
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
