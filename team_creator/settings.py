24fe420d9bf009535b71bb0c34b56b5732de4eb3
Scott Wright <scottwright.qrf@gmail.com>
2022-01-08 15:58:34 -0600

    changes

--- team_creator/settings.py
+++ team_creator/settings.py
@@ -17,7 +17,8 @@ import os.path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 #PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
@@ -127,9 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'v_ball/static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+#STATIC_ROOT= os.path.join(BASE_DIR, 'v_ball/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates', 'static')),
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
