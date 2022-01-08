a0b0c5a886c0895c3261ffc76bf00fc134532c7d
Scott Wright <scottwright.qrf@gmail.com>
2022-01-08 16:11:24 -0600

    changes

--- team_creator/settings.py
+++ team_creator/settings.py
@@ -139,5 +139,4 @@ TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates', 'static')),
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
