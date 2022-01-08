b2bc509f14e9f000b1b537c69a4597e1e5834760
Scott Wright <scottwright.qrf@gmail.com>
2022-01-08 16:00:45 -0600

    changes

--- team_creator/wsgi.py
+++ team_creator/wsgi.py
@@ -11,7 +11,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_creator.settings')
-
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+#application = get_wsgi_application()
 
 
